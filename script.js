da95202c94dd0c1a1e43d7159c62a39a04249d07
Химчик Дмитрий <dimahima14@gmail.com>
2023-03-24 01:44:47 +0300

    v_0.3

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 //Переменные
-const screens = document.querySelectorAll(".screen");
-const board = document.querySelector(".board");
-const gg = document.querySelector(".gg");
-const userScore = document.querySelector(".score");
-const startBTN = document.querySelector("#start");
-let ggPos = 500;
-let score = 0;
+
+const screens = document.querySelectorAll(".screen"),
+  board = document.querySelector(".board"),
+  gg = document.querySelector(".gg"),
+  userScore = document.querySelector(".score"),
+  startBTN = document.querySelector("#start");
+
+let ggPos = 500,
+  score = 0,
+  level = setInterval(() => {
+    score++;
+    userScore.innerHTML = score;
+  }, 5000);
+
+function randome(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
 userScore.innerHTML = score;
 
-//Начало игры
-startBTN.addEventListener("click", (event) => {
-  event.preventDefault();
-  screens[0].classList.add("up");
-});
-let doom = setInterval(() => gameLogic(), 250);
+//Движение
 
-//Игрок
 document.addEventListener("keydown", (event) => {
   switch (event.key) {
     case "ArrowRight":
@@ -35,23 +40,33 @@ document.addEventListener("keydown", (event) => {
   }
 });
 
-//Логика игры
+//Игра
+
+startBTN.addEventListener("click", (event) => {
+  event.preventDefault();
+  screens[0].classList.add("up");
+});
+
+let doom = setInterval(() => gameLogic(), 250);
+
 function gameLogic() {
-  const fireball = document.createElement("div");
-  const { width } = board.getBoundingClientRect();
-  const x = randome(0, width - 70);
-  const y = randome(0, 20);
-  let fireballPosY = y;
+  const fireball = document.createElement("div"),
+    { width } = board.getBoundingClientRect(),
+    x = randome(0, width - 70),
+    y = randome(0, 20);
+
+  let fireball_Y = y;
   fireball.classList.add("fireball");
   fireball.style.width = `70px`;
   fireball.style.height = `120px`;
   fireball.style.top = `${y}px`;
   fireball.style.left = `${x}px`;
-  fireball.style.background = "url(./img/vremenno2.gif)";
+  fireball.style.background = "url(./img/fireball.gif)";
   board.append(fireball);
+
   let end = setInterval(() => {
     if (
-      fireballPosY === 730 &&
+      fireball_Y === 730 &&
       !(ggPos - 32 >= x || ggPos == x || ggPos + 32 <= x)
     ) {
       clearInterval(end);
@@ -59,20 +74,15 @@ function gameLogic() {
       clearInterval(level);
       fireball.remove();
       userScore.innerHTML = score;
-      userScore.parentNode.classList.add("hide");
+      userScore.parentNode.classList.add("opacity");
       board.innerHTML = `<h1>YOU DIE <br/> Ваш счёт: <span class = "primary">${score}<span> <br/>
       <a class="start" onclick = 'window.location.reload();'>заново</a>`;
     } else {
-      fireballPosY++;
-      fireball.style.top = `${fireballPosY}px`;
+      fireball_Y++;
+      fireball.style.top = `${fireball_Y}px`;
     }
-    if (fireballPosY === 900) fireball.remove();
+    if (fireball_Y === 900) fireball.remove();
   }, 5);
-}
-let level = setInterval(() => {
-  score++;
-  userScore.innerHTML = score;
-}, 5000);
-function randome(min, max) {
-  return Math.round(Math.random() * (max - min) + min);
+
+  // lvl 2
 }
